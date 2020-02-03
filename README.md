ee1afe4c974ce1898c830f1d6d5be1057ce2249a
Murat Aydın <44878876+MuratAAydin@users.noreply.github.com>
2020-02-03 15:48:38 +0300

    hot fix
    fixed unprotection

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # strawpall-master
 Fake Strawpall vote
-edit config.json
-if ip protection change ip.txt if you haven't proxy download http://www.live-socks.net/ this link
--- unprotection/index.js
+++ unprotection/index.js
@@ -1,13 +1,12 @@
 const data = require("../config.json");
 const vote = require("strawpoll-bots").default;
-const { join } = require("path");
 
 console.log(`\x1b[33mhave fun\x1b[32m`);
 //edit config.json
 vote(
-  data.protection_url,
-  data.protection_number,
-  true,
-  join(__dirname, "ip.txt"),
-  data.protection_amount
+  data.unprotection_url,
+  data.unprotection_number,
+  false,
+  null,
+  data.unprotection_amount
 );
